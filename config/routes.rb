da2eba379d3b643215005cc263ee20844d4ec97c
rajatNH <rajat@nighthack.in>
2021-06-15 11:10:21 +0530

    add logout

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
+
+  get '/dashboard' => 'user#index'
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'posts#index'
+end
