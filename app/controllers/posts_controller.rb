a62cfd2e4f67afb6a829a720bdde9711bd256a9d
rajatNH <rajat@nighthack.in>
2021-06-15 16:24:37 +0530

    styling index and show post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   # GET /posts/1 or /posts/1.json
@@ -12,7 +12,8 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    @user = User.find_by(email: current_user.email)
+    @post = @user.posts.new
   end
 
   # GET /posts/1/edit
@@ -20,7 +21,10 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @user = User.find_by(id: current_user.id)
+    puts @user.email
+    puts post_params
+    @post = @user.posts.new(post_params)
 
     respond_to do |format|
       if @post.save
